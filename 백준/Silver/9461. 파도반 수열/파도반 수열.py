6f8db1c73aa6b985fb1fc3218e6aa8a2bc2b84a4
TaeyoungPark <mu07010@gmail.com>
2024-12-27 01:16:49 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input().strip())
+
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, 101):
+    dp[i] = dp[i-3] + dp[i-2]
+
+for i in range(T):
+    N = int(input().strip())
+    print(dp[N])

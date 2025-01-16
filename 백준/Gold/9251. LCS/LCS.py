9135274fe1713b42f0d05e5521805536bccb4092
TaeyoungPark <mu07010@gmail.com>
2025-01-16 22:57:56 +0900

    [Gold V] Title: LCS, Time: 468 ms, Memory: 57628 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+a = input().strip()
+b = input().strip()
+
+dp = [[0] * (len(a)+1) for _ in range(len(b)+1)]
+
+for i in range(1, len(b)+1):
+    for j in range(1, len(a)+1):
+        if b[i-1] == a[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])

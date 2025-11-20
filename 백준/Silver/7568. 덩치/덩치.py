6a3a024cc16c52e175aff3ceea42ec06055e7623
TaeyoungPark <mu07010@gmail.com>
2025-11-21 02:58:05 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,17 +1,14 @@
 import sys
 
-input = sys.stdin.readline
+N = int(sys.stdin.readline())
 
-N = int(input().rstrip())
+result = [1]*N
 
-people = [list(map(int, input().split())) for _ in range(N)]
-
-rank = [1] * N
+p = [list(map(int, sys.stdin.readline().split()))  for _ in range(N)]
 
 for i in range(N):
     for j in range(N):
-        if i != j:
-            if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
-                rank[i] += 1
+        if p[i][0] > p[j][0] and p[i][1] > p[j][1]:
+            result[j] += 1
 
-print(" ".join(map(str, rank)))
+print(" ".join(map(str, result)))
